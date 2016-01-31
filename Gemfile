5d3a79c4081b6153f3ff5c3615d4e4b9e4349d47
Polly Sileo <pollysileo@gmail.com>
2016-01-31 10:38:58 -0500

    Add users with devise

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'quiet_assets'
 gem 'pry-rails'
+gem 'devise'
 
 group :development do
   gem 'better_errors'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :appointments
   resources :clients
 
   root 'appointments#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
